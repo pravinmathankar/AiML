795a946a45890b6e25e9435a692610df0b2fd9ae
Pravin mathankar <161911424+pravinmathankar@users.noreply.github.com>
2026-09-22 17:19:48 +0530

    af

--- AIML project/rag.py
+++ AIML project/rag.py
@@ -1,3 +1,262 @@
+# from langchain_core.documents import Document
+
+# sample_doc = Document(
+#     page_content="Hello World!",
+#     metadata={"source": "https://www.google.com"}
+# )
+
+
+# # Text data
+# from langchain_community.document_loaders.text import TextLoader
+
+# loader = TextLoader("data/Python.txt", encoding="utf-8")
+
+# document = loader.load()
+
+# # # PDF data
+# # from langchain_community.document_loaders.pdf import PyPDFLoader
+
+# # pdf_loader = PyPDFLoader("data/research.pdf")
+
+# # document = pdf_loader.load()
+# # document
+
+# # # PDF data
+# # from langchain_community.document_loaders.pdf import PyMuPDFLoader
+
+# # pdf_loader = PyMuPDFLoader("data/research.pdf")
+
+# # document = pdf_loader.load()
+# # document
+
+# # Data => Documents
+# import os
+# from langchain_community.document_loaders.pdf import PyPDFLoader
+
+# def load_all_pdfs():
+#     folder_path = "data/pdfs"
+#     num_docs = 0
+#     all_docs = []
+
+#     for filename in os.listdir(folder_path):
+#         if filename.lower().endswith(".pdf"):
+#             # complete file path
+#             pdf_path = os.path.join(folder_path, filename)
+
+#             loader = PyPDFLoader(pdf_path)
+#             doc = loader.load()
+            
+#             all_docs.extend(doc)
+#             num_docs += 1
+
+#     print("total pdfs:", num_docs)
+#     print("total pages:", len(all_docs))
+#     return all_docs
+
+# all_pdf_documents = load_all_pdfs()
+
+
+# type(all_pdf_documents[1])
+
+
+# from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+# def split_docs(documents, chunk_size=500, chunk_overlap=50):
+    
+#     text_splitter = RecursiveCharacterTextSplitter(
+#         chunk_size = chunk_size,
+#         chunk_overlap = chunk_overlap
+#     )
+
+#     chunked_docs = text_splitter.split_documents(documents)
+#     return chunked_docs
+
+
+# chunks = split_docs(all_pdf_documents)
+
+# from sentence_transformers import SentenceTransformer
+
+
+# class EmbeddingManager:
+#     def __init__(self, model_name="all-MiniLM-L6-v2"):
+        
+#         self.model_name=model_name
+#         print("loading model....", self.model_name)
+#         self.model = SentenceTransformer(self.model_name)
+#         print("embedding dimensions=", self.model.get_sentence_embedding_dimension())
+
+
+#     def generate_embeddings(self, text):
+#         embeddings = self.model.encode(text, show_progress_bar=True)
+#         print("embeddings shape:", embeddings.shape)
+#         return embeddings
+
+
+#     embedding_manager = EmbeddingManager()
+
+
+
+#     import chromadb
+# import uuid
+
+
+# class VectorStoreManager:
+#     def __init__(self, persist_directory="data/vector_store", collection_name="pdf_documents"):
+#         self.collection_name = collection_name
+#         self.persist_directory = persist_directory
+#         self.collection = None
+#         self.client = None
+
+#         self._initialize_store()
+
+#     def _initialize_store(self):
+#         os.makedirs(self.persist_directory, exist_ok=True)
+        
+#         # create a client
+#         self.client = chromadb.PersistentClient(path=self.persist_directory)
+
+#         # create the collection
+#         self.collection = self.client.get_or_create_collection(
+#             name=self.collection_name,
+#             metadata={"description": "vector store collection for pdf embeddings in RAG"}
+#         )
+
+#         print("initialized the vector store with collection:", self.collection_name)
+#         print("docs in collection:", self.collection.count())
+
+#     def add_documents(self, documents, embeddings):
+#         if len(documents) != len(embeddings):
+#             raise ValueError("num of documents does not match num of embeddings")
+
+
+#         # store => ids, embedding, document, metadata
+#         ids = []
+#         all_metadata = []
+#         documents_content = []
+#         embeddings_list = []
+
+#         for i, (doc, embedding) in enumerate(zip(documents, embeddings)):
+#             doc_id = f"doc_{uuid.uuid4()}"
+#             ids.append(doc_id)
+
+#             metadata = dict(doc.metadata)
+#             metadata["doc_index"] = i
+#             metadata["content_length"] = len(doc.page_content)
+#             all_metadata.append(metadata)
+
+#             documents_content.append(doc.page_content)
+
+#             embeddings_list.append(embedding.tolist())
+
+#             self.collection.add(
+#                 ids=ids,
+#                 metadatas=all_metadata,
+#                 documents=documents_content,
+#                 embeddings=embeddings_list
+#             )
+
+#         print("total documents added in vector store=", len(documents_content))
+#         print("docs in collection:", self.collection.count())
+
+
+
+#         vector_store = VectorStoreManager()
+
+
+#         # data => documents => chunks => embeddings => store in vector store
+
+# texts = [doc.page_content for doc in chunks]
+
+# emebedding = embedding_manager.generate_embeddings(texts)
+
+# vector_store.add_documents(chunks, emebedding)
+
+# from sklearn.metrics.pairwise import cosine_similarity
+
+
+# class RAGRetriever:
+#     def __init__(self, embedding_manager, vector_store):
+#         self.embedding_manager = embedding_manager
+#         self.vector_store = vector_store
+
+
+#     def retrieve(self, query, top_k=5, score_threshold=0.0):
+#         # query => embedding
+#         query_embeddings = self.embedding_manager.generate_embeddings([query])[0]
+
+#         # semantic search
+#         results = self.vector_store.collection.query(
+#             query_embeddings=[query_embeddings.tolist()],
+#             n_results=top_k
+#         )
+
+#         # cosine similarity
+#         retrieved_docs=[]
+        
+#         if results["documents"] and results["documents"][0]:
+#             ids = results["ids"][0]
+#             metadatas = results["metadatas"][0]
+#             documents = results["documents"][0]
+#             distances = results["distances"][0]
+
+#             for i, (doc_id, metadata, document, distance) in enumerate(zip(ids, metadatas, documents, distances)):
+#                 similarity_score = 1 - distance
+
+#                 if similarity_score >= score_threshold:
+#                     retrieved_docs.append({
+#                         "id": doc_id,
+#                         "document": document,
+#                         "metadata": metadata,
+#                         "distance": distance,
+#                         "similarity_score": similarity_score,
+#                         "rank" : i + 1
+#                     })
+
+#             print(f"retrieved {len(retrieved_docs)} documents")
+
+#         else:
+#             print("no documents found")
+
+#         return retrieved_docs
+
+
+#     rag_retriever = RAGRetriever(embedding_manager, vector_store)
+
+
+#     rag_retriever.retrieve("What is encoder decoder")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 from langchain_core.documents import Document
 
 sample_doc = Document(
@@ -9,9 +268,11 @@ sample_doc = Document(
 # Text data
 from langchain_community.document_loaders.text import TextLoader
 
-loader = TextLoader("data/Python.txt", encoding="utf-8")
+# loader = TextLoader("data/Python.txt", encoding="utf-8")
+loader = TextLoader("AIML project/Python.txt", encoding="utf-8")
 
 document = loader.load()
+
 
 # # PDF data
 # from langchain_community.document_loaders.pdf import PyPDFLoader
@@ -21,6 +282,7 @@ document = loader.load()
 # document = pdf_loader.load()
 # document
 
+
 # # PDF data
 # from langchain_community.document_loaders.pdf import PyMuPDFLoader
 
@@ -29,9 +291,308 @@ document = loader.load()
 # document = pdf_loader.load()
 # document
 
+
 # Data => Documents
 import os
 from langchain_community.document_loaders.pdf import PyPDFLoader
+
+
+def load_all_pdfs():
+    folder_path = "data/pdfs"
+    num_docs = 0
+    all_docs = []
+
+    for filename in os.listdir(folder_path):
+        if filename.lower().endswith(".pdf"):
+            # complete file path
+            pdf_path = os.path.join(folder_path, filename)
+
+            loader = PyPDFLoader(pdf_path)
+            doc = loader.load()
+
+            all_docs.extend(doc)
+            num_docs += 1
+
+    print("total pdfs:", num_docs)
+    print("total pages:", len(all_docs))
+    return all_docs
+
+
+all_pdf_documents = load_all_pdfs()
+
+type(all_pdf_documents[1])
+
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+def split_docs(documents, chunk_size=500, chunk_overlap=50):
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap
+    )
+
+    chunked_docs = text_splitter.split_documents(documents)
+    return chunked_docs
+
+
+chunks = split_docs(all_pdf_documents)
+
+
+from sentence_transformers import SentenceTransformer
+
+
+class EmbeddingManager:
+    def __init__(self, model_name="all-MiniLM-L6-v2"):
+
+        self.model_name = model_name
+        print("loading model....", self.model_name)
+        self.model = SentenceTransformer(self.model_name)
+        print(
+            "embedding dimensions=",
+            self.model.get_sentence_embedding_dimension()
+        )
+
+    def generate_embeddings(self, text):
+        embeddings = self.model.encode(
+            text,
+            show_progress_bar=True
+        )
+        print("embeddings shape:", embeddings.shape)
+        return embeddings
+
+
+embedding_manager = EmbeddingManager()
+
+
+import chromadb
+import uuid
+
+ 
+
+#vectore directory
+# 
+
+
+class VectorStoreManager:
+    def __init__(
+        self,
+        persist_directory="data/vector_store",
+        collection_name="pdf_documents"
+    ):
+        self.collection_name = collection_name
+        self.persist_directory = persist_directory
+        self.collection = None
+        self.client = None
+
+        self._initialize_store()
+
+    def _initialize_store(self):
+        os.makedirs(self.persist_directory, exist_ok=True)
+
+        # create a client
+        self.client = chromadb.PersistentClient(
+            path=self.persist_directory
+        )
+
+        # create the collection
+        self.collection = self.client.get_or_create_collection(
+            name=self.collection_name,
+            metadata={
+                "description": "vector store collection for pdf embeddings in RAG"
+            }
+        )
+
+        print(
+            "initialized the vector store with collection:",
+            self.collection_name
+        )
+        print(
+            "docs in collection:",
+            self.collection.count()
+        )
+
+    def add_documents(self, documents, embeddings):
+
+        if len(documents) != len(embeddings):
+            raise ValueError(
+                "num of documents does not match num of embeddings"
+            )
+
+        # store => ids, embedding, document, metadata
+        ids = []
+        all_metadata = []
+        documents_content = []
+        embeddings_list = []
+
+        for i, (doc, embedding) in enumerate(
+            zip(documents, embeddings)
+        ):
+            doc_id = f"doc_{uuid.uuid4()}"
+            ids.append(doc_id)
+
+            metadata = dict(doc.metadata)
+            metadata["doc_index"] = i
+            metadata["content_length"] = len(doc.page_content)
+
+            all_metadata.append(metadata)
+
+            documents_content.append(doc.page_content)
+
+            embeddings_list.append(embedding.tolist())
+
+        self.collection.add(
+            ids=ids,
+            metadatas=all_metadata,
+            documents=documents_content,
+            embeddings=embeddings_list
+        )
+
+        print(
+            "total documents added in vector store=",
+            len(documents_content)
+        )
+        print(
+            "docs in collection:",
+            self.collection.count()
+        )
+
+
+vector_store = VectorStoreManager()
+
+
+# data => documents => chunks => embeddings => store in vector store
+
+texts = [doc.page_content for doc in chunks]
+
+emebedding = embedding_manager.generate_embeddings(texts)
+
+vector_store.add_documents(chunks, emebedding)
+
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+class RAGRetriever:
+    def __init__(self, embedding_manager, vector_store):
+        self.embedding_manager = embedding_manager
+        self.vector_store = vector_store
+
+    def retrieve(
+        self,
+        query,
+        top_k=5,
+        score_threshold=0.0
+    ):
+        # query => embedding
+        query_embeddings = self.embedding_manager.generate_embeddings(
+            [query]
+        )[0]
+
+        # semantic search
+        results = self.vector_store.collection.query(
+            query_embeddings=[query_embeddings.tolist()],
+            n_results=top_k
+        )
+
+        # cosine similarity
+        retrieved_docs = []
+
+        if results["documents"] and results["documents"][0]:
+
+            ids = results["ids"][0]
+            metadatas = results["metadatas"][0]
+            documents = results["documents"][0]
+            distances = results["distances"][0]
+
+            for i, (
+                doc_id,
+                metadata,
+                document,
+                distance
+            ) in enumerate(
+                zip(
+                    ids,
+                    metadatas,
+                    documents,
+                    distances
+                )
+            ):
+
+                similarity_score = 1 - distance
+
+                if similarity_score >= score_threshold:
+
+                    retrieved_docs.append({
+                        "id": doc_id,
+                        "document": document,
+                        "metadata": metadata,
+                        "distance": distance,
+                        "similarity_score": similarity_score,
+                        "rank": i + 1
+                    })
+
+            print(
+                f"retrieved {len(retrieved_docs)} documents"
+            )
+
+        else:
+            print("no documents found")
+
+        return retrieved_docs
+
+
+rag_retriever = RAGRetriever(
+    embedding_manager,
+    vector_store
+)
+
+
+rag_retriever.retrieve(
+    "What is encoder decoder"
+)
+
+from langchain_core.documents import Document
+
+sample_doc = Document(
+    page_content="Hello World!",
+    metadata={"source": "https://www.google.com"}
+)
+
+# Text data
+from langchain_community.document_loaders.text import TextLoader
+
+loader = TextLoader("data/Python.txt", encoding="utf-8")
+
+document = loader.load()
+
+
+
+# # PDF data
+# from langchain_community.document_loaders.pdf import PyPDFLoader
+
+# pdf_loader = PyPDFLoader("data/research.pdf")
+
+# document = pdf_loader.load()
+# document
+
+
+# # PDF data
+# from langchain_community.document_loaders.pdf import PyMuPDFLoader
+
+# pdf_loader = PyMuPDFLoader("data/research.pdf")
+
+# document = pdf_loader.load()
+# document
+
+
+
+# Data => Documents
+import os
+from langchain_community.document_loaders.pdf import PyPDFLoader
+
+
 
 def load_all_pdfs():
     folder_path = "data/pdfs"
@@ -53,11 +614,12 @@ def load_all_pdfs():
     print("total pages:", len(all_docs))
     return all_docs
 
+
+
+
 all_pdf_documents = load_all_pdfs()
 
-
 type(all_pdf_documents[1])
-
 
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
@@ -70,7 +632,6 @@ def split_docs(documents, chunk_size=500, chunk_overlap=50):
 
     chunked_docs = text_splitter.split_documents(documents)
     return chunked_docs
-
 
 chunks = split_docs(all_pdf_documents)
 
@@ -92,9 +653,8 @@ class EmbeddingManager:
         return embeddings
 
 
+
     embedding_manager = EmbeddingManager()
-
-
 
     import chromadb
 import uuid
@@ -163,6 +723,7 @@ class VectorStoreManager:
         vector_store = VectorStoreManager()
 
 
+
         # data => documents => chunks => embeddings => store in vector store
 
 texts = [doc.page_content for doc in chunks]
@@ -171,7 +732,11 @@ emebedding = embedding_manager.generate_embeddings(texts)
 
 vector_store.add_documents(chunks, emebedding)
 
+
+
 from sklearn.metrics.pairwise import cosine_similarity
+
+
 
 
 class RAGRetriever:
@@ -223,6 +788,86 @@ class RAGRetriever:
     rag_retriever = RAGRetriever(embedding_manager, vector_store)
 
 
+
     rag_retriever.retrieve("What is encoder decoder")
+
+
+    API_KEY_OPENAI = "paste-your-api-key-here"
+
+
+
+    from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(
+    openai_api_key=API_KEY_OPENAI,
+    model="gpt-5.4",
+    temperature=0.1,
+    max_tokens=1024
+)
+
+# from langchain_openai import ChatOpenAI
+
+# llm = ChatOpenAI(
+#     openai_api_key=API_KEY_OPENAI,
+#     model="gpt-5.4",
+#     temperature=0.1,
+#     max_tokens=1024
+# )
+
+# generate our retrieval-augmented output
+def generate_output(query, retriever, llm, top_k=3):
+    results = retriever.retrieve(query, top_k)
+
+    context = "\n".join([doc["document"] for doc in results]) if results else ""
+
+    if not context:
+        print("we found no relevant context for the given query")
+
+    # context + query
+    prompt = f""" use given context to generate the answer for the query
+                Context: {context}
+                Query: {query} """
+
+    response = llm.invoke(prompt) # expecting a string as prompt
+    return response.content
+
+answer = generate_output("what is encoder-decoder?", rag_retriever, llm)
+
+
+API_Key_GROQ = "paste-your-api-key-here"
+
+
+from langchain_groq import ChatGroq
+
+llm = ChatGroq(
+    groq_api_key=API_Key_GROQ,
+    model="qwen/qwen3-32b",
+    temperature=0.1,
+    max_tokens=1024
+)
+
+
+
+
+# generate our retrieval-augmented output
+def generate_output(query, retriever, llm, top_k=3):
+    results = retriever.retrieve(query, top_k)
+
+    context = "\n".join([doc["document"] for doc in results]) if results else ""
+
+    if not context:
+        print("we found no relevant context for the given query")
+
+    # context + query
+    prompt = f""" use given context to generate the answer for the query
+                Context: {context}
+                Query: {query} """
+
+    response = llm.invoke([prompt.format(context=context, query=query)]) # expecting a list as prompt
+    return response.content
+
+
+
+answer = generate_output("what is RAG?", rag_retriever, llm)
 
 
